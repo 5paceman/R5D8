b03e13f4eede2210d6f53a5b1ae22144c698ef13
5ifth <tom.james15@hotmail.com>
2017-02-01 22:44:49 +0000

    Add project files.

---
+++ R5-D8/Program.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using System;
+using System.Configuration;
+
+namespace R5_D8
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().Start();
+         
+        private DiscordClient theClient;
+
+        public void Start()
+        {
+            theClient = new DiscordClient();
+
+            theClient.ExecuteAndWait(async () =>
+            {
+                await theClient.Connect(ConfigurationSettings.AppSettings["botToken"], TokenType.Bot);
+            });
+        }
+    }
+}
